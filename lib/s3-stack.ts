ebbe9c1302e366df8a6567dbc0a20b0bc590814b
Srinath Bala <srinathbala00@gmail.com>
2022-09-27 18:45:26 +0530

    first commit

--- lib/s3-stack.ts
+++ lib/s3-stack.ts
@@ -1,16 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class S3Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new s3.Bucket(this, 'MyFirstBucket', {
+      bucketName: 'my-first-bucket',
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'S3Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    });
   }
 }
